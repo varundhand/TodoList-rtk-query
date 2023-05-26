3ff5902bf434ffd95b9840c27afae60d4a8ffda4
varundhand <varundhand77@gmail.com>
2023-05-26 18:01:35 +0530

    invalidated cache and refetching working

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,9 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  tagTypes: ["Todos"], //! the data gets cached when we run mutations and we need to INVALIDATE PREVIOUS CACHE in order to update new changes. So we assign tag to the mutations which lets us tell which mutation to invalidate cache and automatically update the data
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => "/todos",
+      providesTags: ["Todos"],
     }),
     addTodo: builder.mutation({
       // mutation is used because we need to mutate the data instead of just query or getting the data
@@ -15,6 +17,7 @@ export const apiSlice = createApi({
         method: "POST",
         body: todo,
       }),
+      invalidatesTags: ["Todos"],
     }),
     updateTodo: builder.mutation({
       query: (todo) => ({
@@ -22,6 +25,7 @@ export const apiSlice = createApi({
         method: "PATCH", // PUT is change the FULL of an existing record whereas PATCH is used to change a PART of an existing resource
         body: todo,
       }),
+      invalidatesTags: ["Todos"],
     }),
     deleteTodo: builder.mutation({
       query: ({ id }) => ({
@@ -30,6 +34,7 @@ export const apiSlice = createApi({
         method: "DELETE",
         body: id,
       }),
+      invalidatesTags: ["Todos"],
     }),
   }),
 });
